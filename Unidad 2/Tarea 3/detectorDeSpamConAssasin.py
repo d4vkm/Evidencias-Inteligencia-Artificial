0c9c2007c378e8a351cfc86704ce37c984d4cd66
Daniel <116851107+d4vkm@users.noreply.github.com>
2025-03-30 21:05:42 +0000

    detectorDeSpamConAssasin

--- Unidad 2/Tarea 3/detectorDeSpamConAssasin.py
+++ Unidad 2/Tarea 3/detectorDeSpamConAssasin.py
@@ -6,22 +6,23 @@ import csv
 
 def leerDataset():
     #Carga el dataset de entrenamiento:
-    with open('/workspaces/Evidencias-Inteligencia-Artificial/Unidad 2/Tarea 3/SMSSpamCollection') as smsSpamCollection:
-        datasetDeEntrenamiento = [line.strip() for line in smsSpamCollection]
+    with open('/workspaces/Evidencias-Inteligencia-Artificial/Unidad 2/Tarea 3/spam_assassin.csv/spam_assassin.csv') as spamAssassin:
+        datasetDeEntrenamiento = [line.strip() for line in spamAssassin]
         print(f"El dataset de entrenamiento tiene {len(datasetDeEntrenamiento)} registros")
 
-    datos = pandas.read_csv('/workspaces/Evidencias-Inteligencia-Artificial/Unidad 2/Tarea 3/SMSSpamCollection',
-                           sep='\t',
-                           quoting=csv.QUOTE_NONE,
-                           names=['class', 'message'])
-    
+    datos = [line.rsplit(',', 1) for line in datasetDeEntrenamiento]
+    df = pandas.DataFrame(datos, columns=["text", "target"])
+    df = df.drop(index=0)
+
+    #df["target"] = pandas.to_numeric(df["target"])
+
     #Imprime los primeros 5 registros:
-    print(datos.head())
+    print(df.head())
 
     #Los clasifica:
-    print(datos.groupby('class').count())
+    print(df.groupby('target').count())
 
-    return datos
+    return df
 
 def formatearPalabras(palabrasDelMensaje):
     mensaje = str(palabrasDelMensaje).lower()
@@ -30,20 +31,20 @@ def formatearPalabras(palabrasDelMensaje):
 
 datos = leerDataset()
 
-vectorDeEntrenamiento=CountVectorizer(analyzer=formatearPalabras).fit(datos.get('message'))
+vectorDeEntrenamiento=CountVectorizer(analyzer=formatearPalabras).fit(datos.get('text'))
 
 #message10 = vectorDeEntrenamiento.transform([datos.get('message')[9]])
 
 #print(message10)
 
 #bolsa de palabras de todo el dataset:
-bolsaDePalabras = vectorDeEntrenamiento.fit_transform(datos.get('message').values)
+bolsaDePalabras = vectorDeEntrenamiento.fit_transform(datos.get('text').values)
 
 #cantidad por palabra (frecuencia por término y frecuencia inversa):
 mensajesTfIdf = TfidfTransformer().fit(bolsaDePalabras).transform(bolsaDePalabras)
 
 #entrenar el modelo:
-detectorDeSpam=MultinomialNB().fit(mensajesTfIdf, datos.get('class').values)
+detectorDeSpam=MultinomialNB().fit(mensajesTfIdf, datos.get('target').values)
 
 #Prueba:
 ejemplo=['FREE CASH $$$ click here for more information']
